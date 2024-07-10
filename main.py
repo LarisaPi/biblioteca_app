2059f507aea4d547e2393530d95b138a00090d8c
larisap <laripinacortez@gmail.com>
2024-07-10 12:57:59 -0600

    Autores

--- main.py
+++ main.py
@@ -24,10 +24,31 @@ def libros():
     #crear un cursor (objeto para recorrer las tablas)#
     cursor=conexion.cursor()
     #ejecutar una consulta en postgres#
-    cursor.execute('''SELECT * FROM libro''') 
+    cursor.execute('''SELECT * FROM libros''') 
     #recuperar la información#
     datos = cursor.fetchall()
     #cerrar cursor y conexion a la base de datos#
     cursor.close()
     conexion.close()
-    return render_template('libro.html', datos=datos)
+    return render_template('libros.html', datos=datos)
+
+@app.route('/autores')
+def autores():
+    #conectar con la base de datos#
+    conexion = psycopg2.connect(
+        database="biblioteca3a",
+        user="postgres",
+        password="lari",
+        host="localhost",
+        port="5432"
+    )
+    #crear un cursor (objeto para recorrer las tablas)#
+    cursor=conexion.cursor()
+    #ejecutar una consulta en postgres#
+    cursor.execute('''SELECT * FROM autores_view''') 
+    #recuperar la información#
+    datos = cursor.fetchall()
+    #cerrar cursor y conexion a la base de datos#
+    cursor.close()
+    conexion.close()
+    return render_template('autores.html', datos=datos)
