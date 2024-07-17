e4778438e0fbacd385d6b61986b847959604e509
larisap <laripinacortez@gmail.com>
2024-07-17 13:00:59 -0600

    Terminar edición y pagina 404

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ bootstrap = Bootstrap(app)
 def index():
     return render_template('base.html')
 
+@app.errorhandler(404)
+def error404(error):
+    return render_template('404.html')
+
 @app.route('/libros')
 def libros():
     #conectar con la base de datos#
@@ -65,7 +69,7 @@ def paises():
     # crear un cursor (objeto para recorrer las tablas)
     cursor = conexion.cursor()
     # ejecutar una consulta en postgres
-    cursor.execute('''SELECT * FROM pais''')
+    cursor.execute('''SELECT * FROM pais ORDER BY id_pais''')
     #recuperar la informacion
     datos = cursor.fetchall()
     #cerrar cursos y conexion a la base de datos
@@ -108,3 +112,21 @@ def update1_pais(id_pais):
     cursor.close()
     conexion.close()
     return render_template('editar_pais.html',datos=datos)
+
+@app.route('/update2_paises/<int:id_pais>', methods=['POST'])
+def update2_pais(id_pais):
+    nombre = request.form['nombre']
+    conexion = psycopg2.connect(
+        database="biblioteca3a",
+        user="postgres",
+        password="lari",
+        host="localhost",
+        port="5432"
+    )
+    # crear un cursor (objeto para recorrer las tablas)
+    cursor = conexion.cursor()
+    cursor.execute('''UPDATE pais SET nombre=%s WHERE id_pais=%s''', (nombre, id_pais,))
+    conexion.commit()
+    cursor.close()
+    conexion.close()
+    return redirect(url_for('index'))
